7f2d7c4c82aeb110a43c3e28b0754af831c44dd5
JanAmbroz <janambroz98@gmail.com>
2024-12-23 17:57:48 +0100

    sixthcommit

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,8 +6,8 @@ async function ProductsPage({
 }: {
   searchParams: { layout?: string; search?: string };
 }) {
-  const layout = (await searchParams)?.layout || "grid";
-  const search = (await searchParams)?.search || "";
+  const layout = searchParams?.layout || "grid";
+  const search = searchParams?.search || "";
   return (
     <>
       <ProductsContainer layout={layout} search={search} />
