45dd88637b4f62c058a62015405a5677dd511981
JanAmbroz <janambroz98@gmail.com>
2024-12-23 15:24:08 +0100

    thirdCommit

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -2,6 +2,8 @@ import { FaHeart } from "react-icons/fa";
 import { Button } from "../ui/button";
 
 function FavoriteToggleButton({ productId }: { productId: string }) {
+  const id = productId;
+  console.log(id);
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />
